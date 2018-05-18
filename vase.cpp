29b69ad2d40c99b4ffd25946e144f282bf1bcaa7
punkey0 <punkey0@gmail.com>
2018-05-17 20:45:55 -0700

    Initial upload of Vase class files

---
+++ vase.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+** Program name: Final Project
+** Author: Kasey Kagawa
+** Date: 11/29/2017
+** Description: Vase Class function definition file
+*********************************************************************/
+
+#include "vase.hpp"
+
+Vase::Vase()
+{
+	name = "Vase";
+	symbol = 'V';
+}
---
+++ vase.hpp
@@ -0,0 +1,20 @@
+/*********************************************************************
+** Program name: Final Project
+** Author: Kasey Kagawa
+** Date: 11/29/2017
+** Description: Vase Class header file
+*********************************************************************/
+
+#include "item.hpp"
+#ifndef VASE_HPP
+#define VASE_HPP
+
+class Vase : public Item
+{
+	private:
+		
+	public:
+		Vase();
+};
+
+#endif
