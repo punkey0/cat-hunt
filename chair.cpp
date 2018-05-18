9a904a188481635619c14b91f16da5e455d264a6
punkey0 <punkey0@gmail.com>
2018-05-17 20:38:09 -0700

    Initial upload of Chair class files

---
+++ chair.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+** Program name: Final Project
+** Author: Kasey Kagawa
+** Date: 11/29/2017
+** Description: Chair Class function definition file
+*********************************************************************/
+
+#include "chair.hpp"
+
+Chair::Chair()
+{
+	name = "Chair";
+	symbol = 'h';
+}
---
+++ chair.hpp
@@ -0,0 +1,20 @@
+/*********************************************************************
+** Program name: Final Project
+** Author: Kasey Kagawa
+** Date: 11/29/2017
+** Description: Chair Class header file
+*********************************************************************/
+
+#include "item.hpp"
+#ifndef CHAIR_HPP
+#define CHAIR_HPP
+
+class Chair : public Item
+{
+	private:
+		
+	public:
+		Chair();
+};
+
+#endif
