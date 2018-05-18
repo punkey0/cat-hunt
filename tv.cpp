15483db16da896035569c7f3c1e07e5b9cdb39d7
punkey0 <punkey0@gmail.com>
2018-05-17 20:45:38 -0700

    Initial upload of TV class files

---
+++ tv.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+** Program name: Final Project
+** Author: Kasey Kagawa
+** Date: 11/29/2017
+** Description: TV Class function definition file
+*********************************************************************/
+
+#include "tv.hpp"
+
+TV::TV()
+{
+	name = "TV";
+	symbol = '@';
+}
---
+++ tv.hpp
@@ -0,0 +1,20 @@
+/*********************************************************************
+** Program name: Final Project
+** Author: Kasey Kagawa
+** Date: 11/29/2017
+** Description: TV Class header file
+*********************************************************************/
+
+#include "item.hpp"
+#ifndef TV_HPP
+#define TV_HPP
+
+class TV : public Item
+{
+	private:
+		
+	public:
+		TV();
+};
+
+#endif
